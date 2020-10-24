03f6697d8ebd27b588cc300bd7481cd6f4bdb757
Sam Warren <51456937+SaWarr@users.noreply.github.com>
2020-10-24 12:32:28 +0100

    Update rpgtextgame.py

--- rpgtextgame.py
+++ rpgtextgame.py
@@ -61,3 +61,203 @@ def help_menu():
     title_screen_selections()
 
 def start_game(): 
+
+#### MAP #### 
+    """
+    Player starts at b2
+    a1 a2 a3 a4 
+    -------------
+    |  |  |  |  | a4
+    -------------
+    |  |  |  |  | b4...
+    -------------
+    |  |  |  |  |
+    -------------
+    |  |  |  |  |
+    -------------
+    """
+
+ZONENAME = ''
+DESCRIPTION = 'description'
+EXAMINATION = 'info'
+SOLVED = False
+UP = 'up', 'north'
+DOWN = 'down', 'south'
+LEFT = 'left', 'west'
+RIGHT = 'right', 'east'
+
+solved_places = {'a1': False, 'a2': False, 'a3': False,'a4': False,
+                'b1': False, 'b2': False, 'b3': False,'b4': False,
+                'c1': False, 'c2': False, 'c3': False,'c4': False,
+                'd1': False, 'd2': False, 'd3': False,'d4': False,
+                }
+
+zone_map = {
+    'a1': {
+        ZONENAME: "Jungle",
+        DESCRIPTION: 'You are surrounded by a lush jungle.',
+        EXAMINATION: 'info',
+        SOLVED: False,
+        UP: '',
+        DOWN: 'b1',
+        LEFT: '',
+        RIGHT: 'a2',
+    },
+    'a2': {
+        ZONENAME: "Beach",
+        DESCRIPTION: 'You are standing on a golden beach.',
+        EXAMINATION: 'info',
+        SOLVED: False,
+        UP: '',
+        DOWN: 'b2',
+        LEFT: 'a1',
+        RIGHT: 'a3',
+    },
+    'a3': {
+        ZONENAME: "Docks",
+        DESCRIPTION: 'You stand on a dock surrounded by a refreshing sea breeze.',
+        EXAMINATION: 'info',
+        SOLVED: False,
+        UP: '',
+        DOWN: 'b3',
+        LEFT: 'a2',
+        RIGHT: 'a4',
+    },
+    'a4': {
+        ZONENAME: "Inn",
+        DESCRIPTION: '',
+        EXAMINATION: 'info',
+        SOLVED: False,
+        UP: '',
+        DOWN: 'b4',
+        LEFT: 'a3',
+        RIGHT: '',
+    },
+    'b1': {
+        ZONENAME: "",
+        DESCRIPTION: 'description',
+        EXAMINATION: 'info',
+        SOLVED: False,
+        UP: 'a1',
+        DOWN: 'c1',
+        LEFT: '',
+        RIGHT: 'b2',
+    },
+    'b2': {
+        ZONENAME: 'Your room',
+        DESCRIPTION: 'This is your bedroom!',
+        EXAMINATION: 'Where your walls used to stand there is greyness, a void. It does not seem harmful.',
+        SOLVED: False,
+        UP: 'a2',
+        DOWN: 'c2',
+        LEFT: 'b1',
+        RIGHT: 'b3',
+    },
+    'b3': {
+        ZONENAME: "",
+        DESCRIPTION: 'description',
+        EXAMINATION: 'info',
+        SOLVED: False,
+        UP: 'a3',
+        DOWN: 'c3',
+        LEFT: 'b2',
+        RIGHT: 'b4',
+    },
+    'b4': {
+        ZONENAME: "",
+        DESCRIPTION: 'description',
+        EXAMINATION: 'info',
+        SOLVED: False,
+        UP: 'a4',
+        DOWN: 'c4',
+        LEFT: 'b3',
+        RIGHT: '',
+    },
+    'c1': {
+        ZONENAME: "",
+        DESCRIPTION: 'description',
+        EXAMINATION: 'info',
+        SOLVED: False,
+        UP: 'b1',
+        DOWN: 'd1',
+        LEFT: '',
+        RIGHT: 'c2',
+    },
+    'c2': {
+        ZONENAME: "",
+        DESCRIPTION: 'description',
+        EXAMINATION: 'info',
+        SOLVED: False,
+        UP: 'b2',
+        DOWN: 'd2',
+        LEFT: 'c1',
+        RIGHT: 'c3',
+    },
+    'c3': {
+        ZONENAME: "",
+        DESCRIPTION: 'description',
+        EXAMINATION: 'info',
+        SOLVED: False,
+        UP: 'b3',
+        DOWN: 'd3',
+        LEFT: 'c2',
+        RIGHT: 'c4',
+    },
+    'c4': {
+        ZONENAME: "",
+        DESCRIPTION: 'description',
+        EXAMINATION: 'info',
+        SOLVED: False,
+        UP: 'b4',
+        DOWN: 'd4',
+        LEFT: 'c3',
+        RIGHT: '',
+    },
+    'd1': {
+        ZONENAME: "",
+        DESCRIPTION: 'description',
+        EXAMINATION: 'info',
+        SOLVED: False,
+        UP: 'c1',
+        DOWN: '',
+        LEFT: '',
+        RIGHT: 'd2',
+    },
+    'd2': {
+        ZONENAME: "",
+        DESCRIPTION: 'description',
+        EXAMINATION: 'info',
+        SOLVED: False,
+        UP: 'c2',
+        DOWN: '',
+        LEFT: 'd1',
+        RIGHT: 'd3',
+    },
+    'd3': {
+        ZONENAME: "",
+        DESCRIPTION: 'description',
+        EXAMINATION: 'info',
+        SOLVED: False,
+        UP: 'c3',
+        DOWN: '',
+        LEFT: 'd2',
+        RIGHT: 'd4',
+    },
+    'd4': {
+        ZONENAME: "",
+        DESCRIPTION: 'description',
+        EXAMINATION: 'info',
+        SOLVED: False,
+        UP: 'c4',
+        DOWN: '',
+        LEFT: 'd3',
+        RIGHT: '',
+    },
+}
+
+#### GAME INTERACTIVITY ####
+
+
+
+
+#### GAME FUNCTIONALITY ####
